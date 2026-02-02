6518a3f95f1c274cdbbe52cd5684634ac0f417f4
zcpefaisal <inboxlions@gmail.com>
2026-02-03 01:40:00 +0600

    feat: initialize ChromaDB persistent client and collection Set up netflix_titles collection with OpenAI embedding function and verify via collection listing.

---
+++ vector-db-chroma/01_getting_started_with_chroma_db.py
@@ -0,0 +1,21 @@
+import os
+import chromadb
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+
+
+# Create a persistant client
+# Note: In a local environment, can pass a path like path="./my_vectordb" 
+# to save data to a specific folder.
+client = chromadb.PersistentClient()
+
+# Create a netflix_title collection using the OpenAI Embedding function
+collection = client.create_collection(
+    name="netflix_titles",
+    embedding_function=OpenAIEmbeddingFunction(
+        model_name="text-embedding-3-small", 
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+)
+
+# List the collections
+print(client.list_collections())
