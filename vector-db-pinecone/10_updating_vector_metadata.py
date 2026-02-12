2699504bbf8dd6b338fc8a65fbb2d7f1ce72cad7
zcpefaisal <inboxlions@gmail.com>
2026-02-12 07:35:33 +0600

    feat: update vector metadata value in Pinecone index

---
+++ vector-db-pinecone/10_updating_vector_metadata.py
@@ -0,0 +1,23 @@
+from pinecone import Pinecone
+
+# Initialize Pinecone
+pc = Pinecone(api_key="pcsk_<TOKEN>rMv9V") 
+index_name = 'my-index'
+
+# Connect to the index
+index = pc.Index(index_name)
+
+# Define your query vector (Matching the index dimension, e.g., 1536)
+vector = [0.1] * 1536 
+
+# Update the values of vector ID item2
+index.update(
+    id="item2", # previously inserted id
+    set_metadata={"category": "Tech", "year": 2025}
+)
+
+# Fetch vector ID item2
+fetched_vector = index.fetch(
+    ids=["item2"]
+)
+print(fetched_vector)
