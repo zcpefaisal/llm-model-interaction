b6f1c68b9166460a71a9d11a52c76cec716ad6f8
zcpefaisal <inboxlions@gmail.com>
2026-03-15 20:18:57 +0600

    feat: Building a retrieval prompt template for create RAG chain

---
+++ llm_lang_chain/15_RAG_build_retrieval_prompt_template_create_RAG_chain.py
@@ -0,0 +1,17 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+# Add placeholders to the message string
+message = """
+Answer the following question using the context provided:
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+# Create a chat prompt template from the message string
+prompt_template = ChatPromptTemplate.from_messages([("human", message)])
